15389da8642ff684fb2f86e502492fe098de4a81
aviFFF <mailme_avishukla@rediffmail.com>
2024-10-27 08:45:42 +0530

    Update TopCategoryList.jsx

--- app/(routes)/products-category/_components/TopCategoryList.jsx
+++ app/(routes)/products-category/_components/TopCategoryList.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 function TopCategoryList({categoryList, selectedCategory} ) {
   return (
-    <div className='flex-col flex gap-5 mt-2 overflow-auto justify-center px-2 md:px-20'>
+    <div className='flex-col flex gap-2 mt-6 overflow-auto justify-center px-2 md:px-20'>
   {categoryList.map((category, index) => (
     <Link 
       key={index} 
       href={'/products-category/' + category?.attributes?.name}
-      className={`flex flex-col items-center justify-center 
-        bg-green-50 md:p-4 group cursor-pointer hover:bg-primary 
-        w-[100px] h-[100px] min-w-[70px] 
+      className={`flex flex-row text-nowrap gap-1 text-left items-center p-1
+        bg-green-50 group cursor-pointer hover:bg-primary 
+        md:w-[170px] md:h-[50px] w-[100px] h-[50px] md:text-sm text-[10px]
         ${selectedCategory == category.attributes.name && 'bg-primary text-white'}
       `}
     >
@@ -21,11 +21,11 @@ function TopCategoryList({categoryList, selectedCategory} ) {
         alt='icon' 
         width={40} 
         height={40}
-        className='group-hover:scale-125 transition-all ease-in-out'
+        className='group-hover:scale-125 w-4 h-4 md:w-auto md:h-auto transition-all ease-in-out'
       />
       
       {/* Smaller text inside the circular category */}
-      <h2 className={`md:text-xs text-[10px] text-center mt-1 text-green-800 group-hover:text-white 
+      <h2 className={`md:text-xs text-[8px] text-center mt-1 text-green-800 group-hover:text-white 
         ${selectedCategory == category?.attributes?.name && 'text-white'}
       `}>
         {category?.attributes?.name}
