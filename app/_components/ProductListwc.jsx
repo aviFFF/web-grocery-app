ae28b54c5b42fa394242c3d8697d54b361df5a3b
aviFFF <mailme_avishukla@rediffmail.com>
2024-10-27 08:51:34 +0530

    Update ProductListwc.jsx

--- app/_components/ProductListwc.jsx
+++ app/_components/ProductListwc.jsx
@@ -3,7 +3,7 @@ import Productitem from './Productitem'
 
 function ProductListwc( {productList}) {
   return (
-    <div className='mt-2 p-2'>
+    <div className='mt-2'>
     <h2 className='text-primary text-2xl text-center mb-4 flex'>  Products You Like</h2>
     <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 '>
         {productList.map((product,index) => index < 8 &&( 
