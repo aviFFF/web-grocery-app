4ecc084b0afbfe2a1e6a2c666dbb339a43f04239
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2025-01-06 18:40:37 +0530

    new url working

--- app/utils/GlobalApi.jsx
+++ app/utils/GlobalApi.jsx
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 const { default: axios } = require("axios");
 
 const axiosClient = axios.create({
-    baseURL:'http://148.135.137.173/api',
+    baseURL:'https://api.buzzat.in/api',
 });
 axiosClient.interceptors.request.use((config) => {
   const token = Cookies.get("token"); // Retrieve token from cookies
