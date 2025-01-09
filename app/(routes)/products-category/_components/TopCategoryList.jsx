2bf86538f78cfc5f4c94fdf51a658c98e3bfc1a2
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2025-01-09 11:58:48 +0530

    productcategoryandtopcategory

--- app/(routes)/products-category/_components/TopCategoryList.jsx
+++ app/(routes)/products-category/_components/TopCategoryList.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 
 function TopCategoryList({categoryList, selectedCategory} ) {
   return (
-    <div className='flex-col flex gap-2 mt-6 overflow-hidden  justify-center px-2 md:px-20 overflow-y-auto'>
+    <div className='flex-col flex gap-2 mt-6 overflow-hidden md:items-start md:justify-start justify-center px-2 md:px-20 overflow-y-auto'>
   {categoryList.map((category, index) => (
     <Link 
       key={index} 
       href={'/products-category/' + category?.attributes?.name}
       className={`flex flex-col md:flex-row p-2
-        bg-green-50 text-center group items-center justify-center cursor-pointer  overflow-hidden
+        bg-green-50 text-center group  items-center justify-center cursor-pointer md:items-start md:justify-start overflow-hidden
         md:w-[190px] md:h-[50px] w-[70px] h-[50px] md:text-sm text-[10px] 
-        ${selectedCategory == category.attributes.name && 'bg-green-300 text-white'}
+        ${selectedCategory == category.attributes.name && 'bg-primary text-primary'}
       `}
     >
       {/* Image inside the circular category */}
@@ -25,8 +25,8 @@ function TopCategoryList({categoryList, selectedCategory} ) {
       />
       
       {/* Smaller text inside the circular category */}
-      <h2 className={`md:text-xs text-[8px] text-center contain mt-1 text-black group-hover:text-white 
-        ${selectedCategory == category?.attributes?.name && 'text-white'}
+      <h2 className={`md:text-xs text-[8px] text-center contain mt-1 text-black group-hover:text-primary 
+        ${selectedCategory == category?.attributes?.name && 'text-primary bg-green-400 '}
       `}>
         {category?.attributes?.name}
       </h2>
