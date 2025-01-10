e6ddc67cd803b6b28a4f6ee0745d2525ca9317e1
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2025-01-11 02:07:12 +0530

    newcheckout1

--- app/success/page.js
+++ app/success/page.js
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-export default function Success() {
-  const router = useRouter();
+// export default function Success() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    toast.success("Payment successful! Your order is placed.");
-    router.replace("/orderPlaced"); // Redirect after confirmation
-  }, [router]);
+//   useEffect(() => {
+//     toast.success("Payment successful! Your order is placed.");
+//     router.replace("/orderPlaced"); // Redirect after confirmation
+//   }, [router]);
 
-  return <div>Processing your successful payment...</div>;
-}
+//   return <div>Processing your successful payment...</div>;
+// }
