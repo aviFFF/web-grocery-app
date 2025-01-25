ea557db88b7af57b95c21de1fc15504ca06bb56b
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2025-01-25 10:08:15 +0530

    oldcheckout

--- app/search/page.js
+++ app/search/page.js
@@ -11,7 +11,6 @@ import {
   DialogHeader,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 // Create a fallback UI to display while loading
@@ -38,46 +37,40 @@ const SearchResults = () => {
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
         {searchResults.length > 0 ? (
           searchResults.map((product) => (
-            <div key={product.id} className="p-4 rounded">
-              <div className="p-1 md:p-2 flex flex-col items-center text-center justify-center gap-3 border rounded-lg hover:shadow-md hover:scale-105 cursor-pointer transition-all ease-in-out">
-                <Image
-                  src={product?.attributes?.image?.data[0]?.attributes?.url}
-                  alt={product?.attributes?.name}
-                  width={500}
-                  height={200}
-                  className="w-[100px] md:h-[100px] h-[50px] object-contain"
-                />
-                <h2 className="font-bold text-sm md:text-lg">
-                  {product?.attributes?.name}
-                </h2>
-                <h2 className="text-gray-500">{product?.attributes?.itemQuantityType}</h2>
-                <div className="flex items-center gap-2">
-                  {product?.attributes?.sellingPrice && (
-                    <h2>₹{product?.attributes?.sellingPrice}</h2>
-                  )}
-                  <h2 className={`font-bold ${product?.attributes?.sellingPrice && 'line-through text-gray-400'}`}>
-                    {product?.attributes?.mrp}
-                  </h2>
+            <Dialog key={product.id}>
+              <DialogTrigger asChild>
+                <div className="p-4 rounded">
+                  <div className="p-1 md:p-2 flex flex-col items-center text-center justify-center gap-3 border rounded-lg hover:shadow-md hover:scale-105 cursor-pointer transition-all ease-in-out">
+                    <Image
+                      src={product?.attributes?.image?.data[0]?.attributes?.url}
+                      alt={product?.attributes?.name}
+                      width={500}
+                      height={200}
+                      className="w-[100px] md:h-[80px] h-[50px] object-contain"
+                    />
+                    <h2 className="font-bold text-sm md:text-lg">
+                      {product?.attributes?.name}
+                    </h2>
+                    <h2 className="text-gray-500">{product?.attributes?.itemQuantityType}</h2>
+                    <div className="flex items-center gap-2">
+                      {product?.attributes?.sellingPrice && (
+                        <h2>₹{product?.attributes?.sellingPrice}</h2>
+                      )}
+                      <h2 className={`font-bold ${product?.attributes?.sellingPrice && 'line-through text-gray-400'}`}>
+                        {product?.attributes?.mrp}
+                      </h2>
+                    </div>
+                  </div>
                 </div>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      className="text-primary rounded-xl hover:text-white hover:bg-primary"
-                      variant="outline"
-                    >
-                      Add To Cart
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogDescription>
-                        <ProductItemDetails product={product} />
-                      </DialogDescription>
-                    </DialogHeader>
-                  </DialogContent>
-                </Dialog>
-              </div>
-            </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogDescription>
+                    <ProductItemDetails product={product} />
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
           ))
         ) : (
           <p>No products found</p>
