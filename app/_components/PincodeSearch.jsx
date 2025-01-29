3833c01a2bdf2b9c9a71bfeee25b243c2b3c8c94
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2025-01-29 18:52:18 +0530

    pincodeupdste

--- app/_components/PincodeSearch.jsx
+++ app/_components/PincodeSearch.jsx
@@ -97,7 +97,7 @@ const PincodeChecker = () => {
 
   return (
     <div className="flex items-center justify-center p-4">
-      <h1 className="text-base md:text-base text-primary">{message}</h1>
+      <h1 className="text-base text-primary">{message}</h1>
     </div>
   );
 };
